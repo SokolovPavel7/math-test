c64129ac26fbb9728f1c1aebc74526914edc3ce6
SokolovPavel7 <sokolovpavel7@gmail.com>
2022-12-26 00:04:19 +0300

    Changed the background

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-let button = document.getElementById("finish");
-button.addEventListener("click", checkTest);
+let button = document.getElementById('finish');
+button.addEventListener('click', checkTest);
 function checkTest() {
   let result = 0;
 
-  let a1 = document.getElementById("q1").value;
-  if (a1 === "6") {
+  let a1 = document.getElementById('q1').value;
+  if (a1 === '6') {
     result++;
   }
-  let a2 = document.getElementById("q2").value;
-  if (a2 === "7") {
+  let a2 = document.getElementById('q2').value;
+  if (a2 === '7') {
     result++;
   }
-  let a3 = document.getElementById("q3").value;
-  if (a3 === "7") {
+  let a3 = document.getElementById('q3').value;
+  if (a3 === '7') {
     result++;
   }
-  let a4 = document.getElementById("q4").value;
-  if (a4 === "3") {
+  let a4 = document.getElementById('q4').value;
+  if (a4 === '3') {
     result++;
   }
-  let a5 = document.getElementById("q5").value;
-  if (a5 === "10") {
+  let a5 = document.getElementById('q5').value;
+  if (a5 === '10') {
     result++;
   }
-  let a6 = document.getElementById("q6").value;
-  if (a6 === "41") {
+  let a6 = document.getElementById('q6').value;
+  if (a6 === '41') {
     result++;
   }
-  return alert("Количество правильных ответов: " + result);
+  return alert('Количество правильных ответов: ' + result);
 }
